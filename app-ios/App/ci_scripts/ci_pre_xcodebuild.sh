475631e76aa25916ea5cefe1492b7907afb59b9e
ryoya ito <ry.itto.github@gmail.com>
2024-08-23 08:48:08 +0900

    fix path

--- app-ios/App/ci_scripts/ci_pre_xcodebuild.sh
+++ app-ios/App/ci_scripts/ci_pre_xcodebuild.sh
@@ -11,11 +11,11 @@ brew install mint
 mint install cybozu/LicenseList
 mint install SwiftGen/SwiftGen
 
-SOURCE_PACKAGES_PATH="$CI_WORKSPACE/app-ios/SourcePackages"
+SOURCE_PACKAGES_PATH="$CI_PRIMARY_REPOSITORY_PATH/app-ios/SourcePackages"
 xcodebuild -resolvePackageDependencies -project $CI_PROJECT_FILE_PATH -scheme DroidKaigi2024App -derivedDataPath $CI_DERIVED_DATA_PATH -clonedSourcePackagesDirPath $SOURCE_PACKAGES_PATH
 
-mint run LicenseList "$CI_WORKSPACE/app-ios/Sources/App/Resources" $SOURCE_PACKAGES_PATH
+mint run LicenseList "$CI_PRIMARY_REPOSITORY_PATH/app-ios/Sources/App/Resources" $SOURCE_PACKAGES_PATH
 
 # generate code
-export DERIVED_SOURCES_DIR="$CI_WORKSPACE/app-ios/Sources/Theme/"
-mint run SwiftGen --config "$CI_WORKSPACE/app-ios/Sources/Theme/swiftgen.yml"
+export DERIVED_SOURCES_DIR="$CI_PRIMARY_REPOSITORY_PATH/app-ios/Sources/Theme/"
+mint run SwiftGen --config "$CI_PRIMARY_REPOSITORY_PATH/app-ios/Sources/Theme/swiftgen.yml"
